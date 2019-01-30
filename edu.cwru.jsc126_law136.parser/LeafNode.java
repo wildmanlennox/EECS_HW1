d11735fbd6061a99f40da1264a066386293dd8c6
Lennox Wildman <wildmanlennox@gmail.com>
2019-01-29 23:19:10 -0500

    Fixed package directory

--- edu.cwru.jsc126_law136.parser/LeafNode.java
+++ edu.cwru.jsc126_law136.parser/LeafNode.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jsc126_law136;
+package edu.cwru.jsc126_law136.parser;
 import java.util.*;
 
 /**
